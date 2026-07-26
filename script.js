816b417273e9a20dda97180832dccc3ea54bf29d
AKHI1928 <a41153016@gmail.com>
2026-07-26 11:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Lucky Nitro Game Engine - Version 1
 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", resizeCanvas);
+
+function gameLoop() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
