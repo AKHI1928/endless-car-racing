3e7dae6af4f406def64e5d104f5d1cf7f45bfb0d
AKHI1928 <a41153016@gmail.com>
2026-07-26 12:00:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,13 @@
-// ===============================
-// Lucky Nitro - Version 1
-// ===============================
+alert("Lucky Nitro JS Loaded");
 
 const canvas = document.getElementById("gameCanvas");
+
+alert(canvas);
+
 const ctx = canvas.getContext("2d");
 
-// Resize Canvas
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
-
-// ===============================
-// Draw Road
-// ===============================
-function drawRoad() {
-
-    // Left Grass
-    ctx.fillStyle = "#081018";
-    ctx.fillRect(0, 0, canvas.width * 0.25, canvas.height);
-
-    // Right Grass
-    ctx.fillRect(canvas.width * 0.75, 0, canvas.width * 0.25, canvas.height);
-
-    // Road
-    ctx.fillStyle = "#222";
-    ctx.fillRect(canvas.width * 0.25, 0, canvas.width * 0.50, canvas.height);
-
-    // Center Line
-    ctx.fillStyle = "#ffffff";
-
-    for (let y = 0; y < canvas.height; y += 80) {
-        ctx.fillRect(canvas.width / 2 - 5, y, 10, 40);
-    }
-}
-
-// ===============================
-// Game Loop
-// ===============================
-function gameLoop() {
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    drawRoad();
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
+ctx.fillStyle = "red";
+ctx.fillRect(100, 100, 200, 200);
